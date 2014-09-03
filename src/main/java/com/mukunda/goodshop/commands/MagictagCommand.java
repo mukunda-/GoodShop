5c111fdfb225f5e55c00c094bf3282b7a7a6f942
mukunda- <mukunda@mukunda.com>
2014-09-03 10:35:30 -0600

    cleanup

--- src/main/java/com/mukunda/goodshop/commands/MagictagCommand.java
+++ src/main/java/com/mukunda/goodshop/commands/MagictagCommand.java
@@ -1,16 +1,9 @@
 package com.mukunda.goodshop.commands;
 
-import java.util.ArrayList;
-
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import com.mukunda.cmdhandler.CommandGroup;
 import com.mukunda.cmdhandler.CommandHandler;
-import com.mukunda.loremeta.LoreMeta;
 
 public class MagictagCommand extends CommandHandler {
 
